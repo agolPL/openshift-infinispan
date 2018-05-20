ff2c948974641d28736d159008e38560b8d49cfe
agolawski <A.Golawski@polish-airports.com>
2018-05-20 23:31:19 +0200

    init test project

---
+++ src/main/java/pl/agol/openshiftinfinispan/OpenshiftInfinispanApplication.java
@@ -0,0 +1,12 @@
+package pl.agol.openshiftinfinispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftInfinispanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftInfinispanApplication.class, args);
+    }
+}
